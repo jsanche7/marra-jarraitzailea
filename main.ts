7c5590db90d8451c77484c9cae8cbdcfa9ced161
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2022-02-21 17:44:31 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,22 @@
 basic.forever(function () {
     if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 38)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
     } else {
         if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 0) {
             DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
-            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 38)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
         }
         if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
-            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 38)
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
             DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
         }
         if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 0) {
-            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 38)
-            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 38)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 100)
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 100)
         }
     }
-    if (DFRobotMaqueenPlus.readPatrol(Patrol.L2) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R2) == 0) {
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 38)
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 38)
+    if (DFRobotMaqueenPlus.readPatrol(Patrol.L2) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R2) == 1) {
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 50)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
     }
 })
